1055335c3a0280b40eff1a294d7b180beaab8c2d
MichalAW <michal.wantaeuco@gmail.com>
2019-06-20 09:30:51 +0200

    add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,72 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const plugins = [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})];
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-       rules: [{
-            test: /\.js$/,
+module.exports = function (env) {
+    if (env === 'production') {
+        plugins.push(
+            new OptimizeJsPlugin({
+                sourceMap: false
+            })
+        )
+    }
+    return {
+        mode: env.production ? 'production' : 'development',
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.bundle.js'
+        },
+        optimization: {
+            minimizer: env.production ? [
+                new TerserPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: true,
+                    terserOptions: {}
+                }),
+            ] : [],
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
                 loader: "babel-loader",
                 options: {
-                    plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [{
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
                 }
-           },
-           {
-               test: /\.css$/,
-               use: [{
-                       loader: 'style-loader'
-                   },
-                   {
-                       loader: 'css-loader',
-                       options: {
-                           modules: true
-                       }
-                   }
-               ]
-           }
-       ]
-    },
-    devServer: {
-        contentBase: './build',
-        publicPath: '/',
-        historyApiFallback: {
-            index: '/index.html'
+            ]
         },
-        port: 3000,
-        hot: true,
-        compress: false,
-        disableHostCheck: true,
+        devServer: {
+            contentBase: './build',
+            publicPath: '/',
+            historyApiFallback: {
+                index: '/index.html'
+            },
+            port: 3000,
+            hot: true,
+            compress: false,
+            disableHostCheck: true,
+        },
+        plugins: plugins
     }
 };
